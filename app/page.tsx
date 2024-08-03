dea591ceec746b715dae1cca04d02dbccb4f8166
Duracell27 <volodymyrshmidt@gmail.com>
2024-08-03 11:26:20 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import WebApp from "@twa-dev/sdk";
+import { useState, useEffect } from "react";
+
+interface UserData {
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  language_code: string;
+  is_premium: boolean;
+}
+export default function Home() {
+  const [userData, setUserData] = useState<UserData | null>(null);
+  useEffect(() => {
+    if (WebApp.initDataUnsafe.user) {
+      setUserData(WebApp.initDataUnsafe.user as UserData);
+    }
+  }, []);
+  return (
+    <main className="p-4">
+      {userData ? (
+        <>
+          <h1 className="text-2xl font-bold mb-4">User data</h1>
+          <ul>
+            <li>ID: {userData.id}</li>
+            <li>First Name: {userData.first_name}</li>
+            <li>Last Name: {userData.last_name || "N/A"}</li>
+            <li>Username: {userData.username || "N/A"}</li>
+            <li>Language Code: {userData.language_code}</li>
+            <li>Premium Status: {userData.is_premium ? "Yes" : "No"}</li>
+          </ul>
+        </>
+      ) : (
+        <>
+          <span>Loading....</span>
+        </>
+      )}
+    </main>
+  );
+}
